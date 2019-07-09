eb851f42dc6670668c0e365f157788fb10f60025
cule219 <culaficstefan@gmail.com>
2019-07-09 11:31:13 +0200

    commit before pull

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router  = express.Router();
 const Article = require('../models/Article');
 
 
-
 /* GET home page */
 router.get('/', (req, res, next) => {
   Article.find({}).then(data =>{
@@ -12,3 +11,28 @@ router.get('/', (req, res, next) => {
 });
 
 module.exports = router;
+
+
+//this was used for category - obsolite atm
+const NewsAPI   = require('newsapi');
+const newsapi = new NewsAPI('d27e647a6e484e358a50c1089f09ecae');
+
+const getNews = ()=>{
+  newsapi.v2.topHeadlines({
+    // sources: 'abc-news',
+    // q: 'bitcoin',
+    category: 'sport',
+    language: 'en',
+    country: 'us'
+  }).then(response => {
+      console.log(response);
+  });
+}
+
+//Category not used anymore
+// router.get('/category/:category', (req, res, next) => {
+//   Article.find({category: req.params.category}).then(data =>{
+//     console.log(data)
+//     res.render('index', {data});
+//   }).catch(err=>console.log(err));
+// });
--- routes/profiles.js
+++ routes/profiles.js
@@ -2,64 +2,28 @@ const express   = require('express')
 const router    = express.Router();
 const Source    = require('../models/Source');
 const Articles  = require('../models/Article');
-const NewsAPI   = require('newsapi');
+const User      = require('../models/User');
 
-const newsapi = new NewsAPI('d27e647a6e484e358a50c1089f09ecae');
 
 router.get('/source/:id', (req, res) =>{
   Source.find({'id': req.params.id}).then(data =>{
     Articles.find({'source.id': req.params.id}).then(articles =>{
-      console.log(data)
       res.render('profile/source', {data, articles});
     });
   }).catch(err=>console.log(err));
 });
 
-router.get('/userProfile/:id', (req, res, next) => {
-  User.find({'_id': req.params.id}).then(data => {
-    res.render('profile/user', {data});
+router.get('/profile/user/:id',(req, res) => {
+  User.findById({_id: req.params.id}).then(data => {
+    let user = false; if(req.params.id == req.user._id) user =true;
+    res.render('profile/user', {data, user});
   });
-});
+})
 
+//Pauls test route
 router.get('/profile/user',(req, res) => {
   const { user } = req.params
   res.render('profile/user')
 })
 
-router.get('/profile/source/:id', (req, res) => {
-  const { source } = req.params
-
-  res.render('homepages/sources/:id', { source })
-})
-
-
 module.exports = router
-
-
-// router.get('/source/:id', (req, res) =>{
-//   newsapi.v2.topHeadlines({
-//     sources: req.params.id, // id
-//     // q: 'bitcoin',
-//     // category: 'business',
-//     language: 'en',
-//     // country: 'us'
-//   }).then(response => {
-//     let articles = response.articles;
-//     articles.forEach((element, index) => {
-//       Articles.findOne({title: element.title}), (err, article) =>{
-//         if(article === null){
-//           console.log(article);
-//           Articles.insertOne({source, author, title, description, url, urlToImage, publishedAt, content} = element);
-//         }
-//         else{
-//           articles.splice(index, 1, element);
-//         }
-//       }
-//     });
-//     Articles.find
-//     Source.find({'id': req.params.id}).then(data =>{
-//       res.render('profile/source', {data, articles});
-//     });
-//   }).catch(err=>(console.log(err)));
-  
-// });
