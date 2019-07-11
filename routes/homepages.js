295d1a4f2a9e84c055b6ff3033352dafc1596198
cule219 <culaficstefan@gmail.com>
2019-07-11 19:10:28 +0200

    thanks montasar

--- routes/homepages.js
+++ routes/homepages.js
@@ -13,7 +13,7 @@ router.get('/homepages/sources', (req, res) => {
 router.get('/article/:articleId', (req, res, next) => {
   let liked = false;
   Article.findOne({'_id': req.params.articleId }, (err, doc)=>{
-    if(req.user._id == undefined){}
+    if(req.user == undefined){}
     else if(doc.ratings.indexOf(req.user._id) !== -1)liked=true;
   }).populate({path: 'comments', populate: {path: 'author'}}).then(article =>{
     article.title = article.title.substring(0, article.title.lastIndexOf('-'));
