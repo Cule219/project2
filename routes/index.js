e9cb8ce3405e87f0e526c38d252c88f8af08bbc6
paul <paulosullivan22@outlook.com>
2019-07-12 13:10:39 +0200

    reverting revert

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,15 @@ const Article     = require('../models/Article');
 /* GET home page */
 router.get('/', (req, res, next) => {
   Article.find({}).then(data =>{
+    
+    if (data[data.length - 1].source.name === 'Showbiz411.com') {
+      data.pop();
+    }
+
     data.forEach(el => {
       el.title = el.title.substring(0, el.title.lastIndexOf('-'))
       el.publishDate = el.publishedAt.toDateString()
     })
-    // if (data[data.length - 1].source.name === 'Showbiz411.com') {
-    //   data.pop();
-    // }
     let first = data.pop();
     res.render('index', {data, first, user: req.user});
   }).catch(err=>console.log(err));
