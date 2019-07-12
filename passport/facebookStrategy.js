3ac5d3cc4dac18c5f561dbff57515a1c74ce271d
cule219 <culaficstefan@gmail.com>
2019-07-13 00:50:16 +0200

    commit before merge

--- passport/facebookStrategy.js
+++ passport/facebookStrategy.js
@@ -2,23 +2,23 @@ const FacebookStrategy  = require("passport-facebook").Strategy;
 const passport          = require('passport');
 const User              = require('../models/User');
 
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_USER_ID,
-    clientSecret: process.env.FACEBOOK_KEY,
-    callbackURL: "http://opinion-news.herokuapp.com/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOne({ facebookId: profile.id }).then(user => {
-      if (user) return cb(null, user);
-      console.log('FB user: ' + user)
-      return User.create({
-        facebookId: profile.id,
-        fullName: profile.displayName
-      }).then(newUser => {
-        return cb(null, newUser);
-      });
-    }).catch(err => {
-      cb(err);
-    });
-  }
-));
+// passport.use(new FacebookStrategy({
+//     clientID: process.env.FACEBOOK_USER_ID,
+//     clientSecret: process.env.FACEBOOK_KEY,
+//     callbackURL: "http://opinion-news.herokuapp.com/auth/facebook/callback"
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//     User.findOne({ facebookId: profile.id }).then(user => {
+//       if (user) return cb(null, user);
+//       console.log('FB user: ' + user)
+//       return User.create({
+//         facebookId: profile.id,
+//         fullName: profile.displayName
+//       }).then(newUser => {
+//         return cb(null, newUser);
+//       });
+//     }).catch(err => {
+//       cb(err);
+//     });
+//   }
+// ));
