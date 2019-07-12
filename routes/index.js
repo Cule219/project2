8b302cf96946521e74b712dd189f652f6c9acded
paul <paulosullivan22@outlook.com>
2019-07-12 13:05:01 +0200

    resolved first image not appearing

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ router.get('/', (req, res, next) => {
       el.title = el.title.substring(0, el.title.lastIndexOf('-'))
       el.publishDate = el.publishedAt.toDateString()
     })
-    if (data[0].source.name === 'Showbiz411.com') data.pop();
+    if (data[data.length - 1].source.name === 'Showbiz411.com') {
+      data.pop();
+    }
     let first = data.pop();
     res.render('index', {data, first, user: req.user});
   }).catch(err=>console.log(err));
