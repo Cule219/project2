60579ee782260e661c7fd71b87a37569a244d13c
Cule219 <culaficstefan@gmail.com>
2020-02-24 21:15:30 -0500

    test google passport

--- passport/googleStrategy.js
+++ passport/googleStrategy.js
@@ -1,22 +1,23 @@
-const GoogleStrategy  = require('passport-google-oauth20').Strategy;
-const passport        = require('passport');
-const User            = require('../models/User');
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const passport = require("passport");
+const User = require("../models/User");
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
-},
-function(accessToken, refreshToken, profile, cb) {
-  User.findOne({ googleId: profile.id })
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "https://opinion-news.herokuapp.com/auth/google/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      User.findOne({ googleId: profile.id })
         .then(user => {
           if (user) return cb(null, user);
-          return User.create(
-            { 
+          return User.create({
             googleId: profile.id,
             profileImg: profile.photos.value,
             username: profile.displayName
-            }).then(newUser => {
+          }).then(newUser => {
             return cb(null, newUser);
           });
         })
@@ -24,7 +25,8 @@ function(accessToken, refreshToken, profile, cb) {
           cb(err);
         });
     }
-));
+  )
+);
 
 //Google profile examp:
 
